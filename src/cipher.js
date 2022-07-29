bc446797e64846768597085477ec7baa8d905910
AngelMelo12 <angellmello1993@gmail.com>
2022-07-29 19:39:34 -0300

    Commit inicial

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
 const cipher = {
   // ...
-};
+  encode: function(offset, texto){
+
+
+  },
+
+  decode: function(offset, texto) {
+    // aqui vai a logica de decifrar
+
+  }
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function cifrar() {
+    let texto = document.getElementById("texto1").value.toUpperCase()
+    let offset = document.getElementById("offset").value
+    let resposta = document.getElementById("respostaCifrada")
+    resposta.innerText = "Sua frase cifrada é: " 
+    window.alert(texto)
+}
+
+function decifrar() {
+    let texto = document.getElementById("texto2").value.toUpperCase()
+    let offset = document.getElementById("offset").value
+    let resposta = document.getElementById("respostaDecifrada")
+    resposta.innerText = "Sua frase decifrada é: " 
+    window.alert(offset)
+}
+
+document.getElementById("botaoCifrar").addEventListener('click', cifrar)
+document.getElementById("botaoDecifrar").addEventListener('click', decifrar)
+
