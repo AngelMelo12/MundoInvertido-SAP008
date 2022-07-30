7ebba9918b637c1ad50a569e388bae4c671b56d3
AngelMelo12 <angellmello1993@gmail.com>
2022-07-30 19:13:34 -0300

    Adicionei a lógica do Code e Encode.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,37 @@ import cipher from './cipher.js';
 
 function cifrar() {
     let texto = document.getElementById("texto1").value.toUpperCase()
-    let offset = document.getElementById("offset").value
+    let offset =parseInt(document.getElementById("offset").value)
     let resposta = document.getElementById("respostaCifrada")
-    resposta.innerText = "Sua frase cifrada é: " 
-    window.alert(texto)
+    let textoCodificado = ""
+
+    for (let i = 0 ; i< texto.length ; i++) {
+        let codigoAscii = texto.charCodeAt(i)
+        let codigoAsciiEoffset = (((codigoAscii + offset) -65) % 26) + 65
+        let resultado = String.fromCharCode(codigoAsciiEoffset)
+        textoCodificado += resultado
+    }
+
+
+    resposta.innerText = "Sua frase cifrada é: " + textoCodificado 
+   
 }
 
 function decifrar() {
     let texto = document.getElementById("texto2").value.toUpperCase()
     let offset = document.getElementById("offset").value
     let resposta = document.getElementById("respostaDecifrada")
-    resposta.innerText = "Sua frase decifrada é: " 
-    window.alert(offset)
+    let textoDecodificado = ""
+
+    for (let i = 0 ; i< texto.length ; i++) {
+        let codigoAscii = texto.charCodeAt(i)
+        let codigoAsciiEoffset = (((codigoAscii - offset) -90) % 26) + 90
+        let resultado = String.fromCharCode(codigoAsciiEoffset)
+        textoDecodificado += resultado
+    }
+
+    resposta.innerText = "Sua frase decifrada é: " + textoDecodificado 
+    
 }
 
 document.getElementById("botaoCifrar").addEventListener('click', cifrar)
