c3c4ad83dce876d46b28fe789f553c279ab292d9
AngelMelo12 <angellmello1993@gmail.com>
2022-07-30 20:10:00 -0300

    Realoquei o código do Code e Encode no cipher.

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,31 @@
 const cipher = {
-  // ...
+  
   encode: function(offset, texto){
+    let textoCodificado = ""
+    for (let i = 0 ; i< texto.length ; i++) {
+      let codigoAscii = texto.charCodeAt(i)
+      let codigoAsciiEoffset = (((codigoAscii + offset) -65) % 26) + 65
+      let resultado = String.fromCharCode(codigoAsciiEoffset)
+      textoCodificado += resultado
+    }
 
+    return textoCodificado
 
   },
 
-  decode: function(offset, texto) {
-    // aqui vai a logica de decifrar
+  decode: function(offset, texto){
+    let textoDecodificado = ""
+    for (let i = 0 ; i< texto.length ; i++) {
+      let codigoAscii = texto.charCodeAt(i)
+      let codigoAsciiEoffset = (((codigoAscii - offset) -90) % 26) + 90
+      let resultado = String.fromCharCode(codigoAsciiEoffset)
+      textoDecodificado += resultado
+
+      return textoDecodificado
+  }
 
   }
 }
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -4,17 +4,8 @@ function cifrar() {
     let texto = document.getElementById("texto1").value.toUpperCase()
     let offset =parseInt(document.getElementById("offset").value)
     let resposta = document.getElementById("respostaCifrada")
-    let textoCodificado = ""
-
-    for (let i = 0 ; i< texto.length ; i++) {
-        let codigoAscii = texto.charCodeAt(i)
-        let codigoAsciiEoffset = (((codigoAscii + offset) -65) % 26) + 65
-        let resultado = String.fromCharCode(codigoAsciiEoffset)
-        textoCodificado += resultado
-    }
-
-
-    resposta.innerText = "Sua frase cifrada é: " + textoCodificado 
+    
+    resposta.innerText = "Sua frase cifrada é: " + cipher.encode(offset, texto)
    
 }
 
@@ -22,16 +13,8 @@ function decifrar() {
     let texto = document.getElementById("texto2").value.toUpperCase()
     let offset = document.getElementById("offset").value
     let resposta = document.getElementById("respostaDecifrada")
-    let textoDecodificado = ""
-
-    for (let i = 0 ; i< texto.length ; i++) {
-        let codigoAscii = texto.charCodeAt(i)
-        let codigoAsciiEoffset = (((codigoAscii - offset) -90) % 26) + 90
-        let resultado = String.fromCharCode(codigoAsciiEoffset)
-        textoDecodificado += resultado
-    }
-
-    resposta.innerText = "Sua frase decifrada é: " + textoDecodificado 
+   
+    resposta.innerText = "Sua frase decifrada é: " + cipher.decode(offset, texto)
     
 }
 
