0f7bacf6061aa658d15af92882751c2117bf566d
AngelMelo12 <angellmello1993@gmail.com>
2022-08-01 12:38:31 -0300

    Validação do TypeError e melhoria do código.

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,23 @@
 const cipher = {
   
-  encode: function(offset, texto){
+  encode: function(offset, texto) {
     let textoCodificado = ""
-    for (let i = 0 ; i< texto.length ; i++) {
+  
+    if(typeof offset != 'number' || typeof texto != 'string' || 
+      typeof offset === 'undefined' || typeof texto === 'undefined') {
+        
+      throw TypeError()
+    }
+
+    for (let i = 0 ; i < texto.length ; i++) {
       let codigoAscii = texto.charCodeAt(i)
-      let codigoAsciiEoffset = (((codigoAscii + offset) -65) % 26) + 65
+
+      if(codigoAscii == 32) {
+        textoCodificado += " "
+        continue
+      }
+
+      let codigoAsciiEoffset = (((codigoAscii + offset) - 65) % 26) + 65
       let resultado = String.fromCharCode(codigoAsciiEoffset)
       textoCodificado += resultado
     }
@@ -13,20 +26,31 @@ const cipher = {
 
   },
 
-  decode: function(offset, texto){
+  decode: function(offset, texto) {
     let textoDecodificado = ""
-    for (let i = 0 ; i< texto.length ; i++) {
+
+    if(typeof offset != 'number' || typeof texto != 'string' || 
+      typeof offset === 'undefined' || typeof texto === 'undefined') {
+
+      throw TypeError()
+    }
+
+    for (let i = 0 ; i < texto.length ; i++) {
       let codigoAscii = texto.charCodeAt(i)
-      let codigoAsciiEoffset = (((codigoAscii - offset) -90) % 26) + 90
+
+      if(codigoAscii == 32) {
+        textoDecodificado += " "
+        continue
+      }
+
+      let codigoAsciiEoffset = (((codigoAscii - offset) - 90) % 26) + 90
       let resultado = String.fromCharCode(codigoAsciiEoffset)
       textoDecodificado += resultado
-    
-  }
+    }
 
-  return textoDecodificado
+    return textoDecodificado
 
   }
 }
 
 export default cipher;
-
